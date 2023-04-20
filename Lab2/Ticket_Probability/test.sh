6a473b0cc29f1161821f1e422d039d45120772a7
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 12:33:31 -0400

    Update test.sh

--- Lab2/Ticket_Probability/test.sh
+++ Lab2/Ticket_Probability/test.sh
@@ -2,5 +2,5 @@
 source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /ticket_prob/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /ticket_prob/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal parking-violation-2022.csv /ticket_prob/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking-violation-2022.csv /ticket_prob/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_prob.py hdfs://$SPARK_MASTER:9000/ticket_prob/input/
