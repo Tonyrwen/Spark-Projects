c33f7efd1d11aa435f47a1460be705f063c88871
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 16:04:01 -0400

    Add files via upload

--- Lab2/Comfort_Zone/comfort_zone.py
+++ Lab2/Comfort_Zone/comfort_zone.py
@@ -1,26 +1,89 @@
 from __future__ import print_function
+from pyspark.ml.feature import VectorAssembler, StandardScaler
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.sql.functions import when, col, min, max, avg
 from pyspark.sql import SparkSession
+import numpy as np
 import sys
 
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Comfort_Zone_KMean").getOrCreate()
+spark = SparkSession.builder.appName("Comfort_Zone_KMean").getOrCreate()
+
+df = spark.read.format("csv").option("header", "true").load(sys.argv[1])
+
+# data preprocessing
+## drop irrelevant columns, type correct, and drop NAs
+df = \
+df.select("player_id", "player_name", "SHOT_DIST", "CLOSE_DEF_DIST", "SHOT_CLOCK", "SHOT_RESULT")\
+  .withColumn('SHOT_RESULT', when(df.SHOT_RESULT == "made", 1)\
+                            .when(df.SHOT_RESULT == "missed", 0)\
+                            .otherwise(None))\
+  .withColumns({'player_id': col("player_id").cast("int"),
+                'SHOT_DIST': col("SHOT_DIST").cast('double'),
+                'CLOSE_DEF_DIST': col("CLOSE_DEF_DIST").cast('double'),
+                'SHOT_CLOCK': col("SHOT_CLOCK").cast('double')})\
+  .na.drop()
+
+## get player average stats
+cz_df = \
+df.groupBy("player_id").avg('SHOT_DIST', 'CLOSE_DEF_DIST', 'SHOT_CLOCK')\
+  .withColumnsRenamed({'avg(SHOT_DIST)':'MEAN_SHOT_DIST',
+                       'avg(CLOSE_DEF_DIST)':'MEAN_CLOSE_DEF_DIST',
+                       'avg(SHOT_CLOCK)':'MEAN_SHOT_CLOCK',})
+  
+## create features column and standardize it
+assemble = VectorAssembler(inputCols = ['MEAN_SHOT_DIST','MEAN_CLOSE_DEF_DIST','MEAN_SHOT_CLOCK'],
+                           outputCol = 'features')
+scaler = StandardScaler(inputCol = 'features', outputCol = 'standardized')
+cz_df = assemble.transform(cz_df)
+cz_df = scaler.fit(cz_df).transform(cz_df)
+
+# kmean instantiate, fit, predict
+kmeans = KMeans(featuresCol = 'standardized').setK(4).setSeed(1)
+model = kmeans.fit(cz_df)
+output = model.transform(cz_df)
+
+# evaluate cluster quality
+silhouette = ClusteringEvaluator().evaluate(output)
+print('**************************************************\n')
+print("Silhouette with squared euclidean distance = " + str(silhouette))
+
+# check cluster stat range
+clst_df = \
+output.groupby('prediction')\
+      .agg(min("MEAN_SHOT_DIST").alias("MIN_SHOT_DIST"),max("MEAN_SHOT_DIST").alias("MAX_SHOT_DIST"), avg("MEAN_SHOT_DIST").alias("MEAN_SHOT_DIST"), 
+           min("MEAN_CLOSE_DEF_DIST").alias("MIN_CLOSE_DEF_DIST"),max("MEAN_CLOSE_DEF_DIST").alias("MAX_CLOSE_DEF_DIST"),avg("MEAN_CLOSE_DEF_DIST").alias("MEAN_CLOSE_DEF_DIST"),
+           min("MEAN_SHOT_CLOCK").alias("MIN_SHOT_CLOCK"),max("MEAN_SHOT_CLOCK").alias("MAX_SHOT_CLOCK"),avg("MEAN_SHOT_CLOCK").alias("MEAN_SHOT_CLOCK"),)
+
+print("Comfort Zone Cluster Profile:\n")
+clst_df.show()
+print('\n\nPlayer of Interest Best Zone and Accuracy:')
+
+# get player of interest best zone based on accuracy
+poi_lst = ["james harden", 'chris paul', "stephen curry", "lebron james"]
+for player in poi_lst:
+  zone_performance = []
+  for cluster in clst_df.collect():
+    player_df = \
+    df.filter((df.player_name == player) &\
+              (df.SHOT_DIST.between(cluster.MIN_SHOT_DIST, cluster.MAX_SHOT_DIST)) &\
+              (df.CLOSE_DEF_DIST.between(cluster.MIN_CLOSE_DEF_DIST, cluster.MAX_CLOSE_DEF_DIST)) &\
+              (df.SHOT_CLOCK.between(cluster.MIN_SHOT_CLOCK, cluster.MAX_SHOT_CLOCK)))
     
-    # load in dataset
-    df = spark.read.format("csv").options("header", "true").load(sys.argv[1])
-    df.show(n=3)
+    # must have enough shot volume
+    if player_df.count() < 10: continue
     
-    # preprocessing
-    
-    
-    # train k-means model
-    
-    
-    # evaluate
-    
-    
-    # show poi cluster
-    
-    
-    spark.stop()
+    zone_performance.append(sum(player_df.select('SHOT_RESULT').toPandas()['SHOT_RESULT'])/player_df.count())
+
+  zone = int(np.argmax(zone_performance))
+  #print(player, zone)
+  zone_row = clst_df.filter(clst_df.prediction == zone).select('MEAN_SHOT_DIST', "MEAN_CLOSE_DEF_DIST", "MEAN_SHOT_CLOCK")
+
+  print("{}:\tZone {} - [{:.1f}, {:.1f}, {:.1f}]\tAccuracy: {:.1f}%".format(player, zone, 
+                        zone_row.first()['MEAN_SHOT_DIST'], 
+                        zone_row.first()['MEAN_CLOSE_DEF_DIST'],
+                        zone_row.first()['MEAN_SHOT_CLOCK'],
+                        np.max(zone_performance)*100
+                        ))
+print('**************************************************')
+spark.stop()
