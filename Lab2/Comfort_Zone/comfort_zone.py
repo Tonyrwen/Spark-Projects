6198ba76487732ca46880d1d057f961f95c5b9e5
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 12:20:02 -0400

    Create comfort_zone.py

---
+++ Lab2/Comfort_Zone/comfort_zone.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.sql import SparkSession
+import sys
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Comfort_Zone_KMean").getOrCreate()
+    
+    # load in dataset
+    df = spark.read.format("csv").options("header", "true").load(sys.argv[1])
+    df.show(n=3)
+    
+    # preprocessing
+    
+    
+    # train k-means model
+    
+    
+    # evaluate
+    
+    
+    # show poi cluster
+    
+    
+    spark.stop()
