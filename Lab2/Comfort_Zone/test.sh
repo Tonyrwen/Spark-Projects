0039629d8c318d84cd2fa306681087a6f69b4eb0
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 19:22:50 -0400

    integrated start.sh and stop.sh

--- Lab2/Comfort_Zone/test.sh
+++ Lab2/Comfort_Zone/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source ../../env.sh
+../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /comfort_zone/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /comfort_zone/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal shot_logs.csv /comfort_zone/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./comfort_zone.py hdfs://$SPARK_MASTER:9000/comfort_zone/input/
+../../stop.sh
