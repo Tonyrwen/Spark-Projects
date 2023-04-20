e0598603b6114d04e5320065f7f40438f2fe383d
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 19:24:19 -0400

    integrated start.sh and stop.sh

--- Lab2/Ticket_Time/test.sh
+++ Lab2/Ticket_Time/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source ../../env.sh
+../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /ticket_time/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /ticket_time/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking-violation-2022.csv /ticket_time/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/
+../../stop.sh
