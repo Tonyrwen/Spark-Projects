0b5a02a6d9faa465b63763da88ffedce418093d4
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 12:33:12 -0400

    Create test.sh

---
+++ Lab2/Ticket_Time/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source ../../env.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /ticket_time/input/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /ticket_time/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking-violation-2022.csv /ticket_time/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/
