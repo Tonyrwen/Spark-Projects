91d9016c5340ccf50cded42dcead3fc5eb8683df
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-20 23:42:30 -0400

    Update test.sh

--- Lab2/Ticket_Time/test.sh
+++ Lab2/Ticket_Time/test.sh
@@ -4,5 +4,12 @@ source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /ticket_time/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /ticket_time/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking-violation-2022.csv /ticket_time/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/
+echo "\n\nParallelism 2:\n"
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/ --conf spark.default.parallelism=2
+echo "\n\nParallelism 3:\n"
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/ --conf spark.default.parallelism=3
+echo "\n\nParallelism 4:\n"
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/ --conf spark.default.parallelism=4
+echo "\n\nParallelism 5:\n"
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/ --conf spark.default.parallelism=5
 ../../stop.sh
