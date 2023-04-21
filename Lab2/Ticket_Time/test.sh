fa1a8e0561307a1106687c024b693f9f9a345603
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-04-21 00:07:16 -0400

    Update test.sh

--- Lab2/Ticket_Time/test.sh
+++ Lab2/Ticket_Time/test.sh
@@ -3,7 +3,7 @@ source ../../env.sh
 ../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /ticket_time/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /ticket_time/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking-violation-2022.csv /ticket_time/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking-violation-2023.csv /ticket_time/input/
 echo "\n\nParallelism 2:\n"
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./ticket_time.py hdfs://$SPARK_MASTER:9000/ticket_time/input/ --conf spark.default.parallelism=2
 echo "\n\nParallelism 3:\n"
