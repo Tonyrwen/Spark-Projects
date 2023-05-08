02c418ef6c143c1ccac418444b0ece8785f6b076
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-05-08 00:07:56 -0400

    Update test.sh

--- Project2/Part1/test.sh
+++ Project2/Part1/test.sh
@@ -8,6 +8,6 @@ source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal test.csv /toxic_comment/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal test_labels.csv /toxic_comment/input/
 
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./toxic_comment_lr.py hdfs://$SPARK_MASTER:9000/toxic_comment/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./toxic_comment_lr.py hdfs://$SPARK_MASTER:9000/toxic_comment/input/train.csv hdfs://$SPARK_MASTER:9000/toxic_comment/input/test.csv hdfs://$SPARK_MASTER:9000/toxic_comment/input/test_labels.csv
 
 ../../stop.sh
